aff4b2bb0dcf4d5838f1b7c3ce30402632eafef1
EphriamHab <ephremhabtamu2015@gmail.com>
2024-11-18 20:40:43 +0300

    vercel.json file added

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
 
 // Route: /name
-app.get('/name', (req, res) => {
-    res.status(200).send('Ephrem Habtamu.');
+app.get("/name", (req, res) => {
+  res.status(200).send("Ephrem Habtamu.");
 });
 
 // Route: /hobby
-app.get('/hobby', (req, res) => {
-    res.status(200).json({ hobby: 'Reading book and Listening music' });
+app.get("/hobby", (req, res) => {
+  res.status(200).json({ hobby: "Reading book and Listening music" });
 });
 
 // Route: /dream
-app.get('/dream', (req, res) => {
-    res.status(200).send('My dream is to build impactful software solutions that change lives.');
+app.get("/dream", (req, res) => {
+  res
+    .status(200)
+    .send(
+      "My dream is to build impactful software solutions that change lives."
+    );
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://0.0.0.0:${port}`);
+  console.log(`Server is running at http://0.0.0.0:${port}`);
 });
